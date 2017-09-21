d0919c0f2da8d32c9198dffddad66eeae4b9dc42
samyoshon <samyoshon@gmail.com>
2017-09-20 22:05:53 -0700

    show emoji

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,7 @@ class Product < ActiveRecord::Base
 	mount_uploaders :images, ImageUploader
 	accepts_nested_attributes_for :images
 
-	validates :title, presence: true, :email_format => true
+	# validates :title, presence: true, :email_format => true
 	# # validates :title, presence: true
 	# validates :category_id, presence: true
 	# validates :price, presence: true
