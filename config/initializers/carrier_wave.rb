759fc8e64080fbe3f9e25e948dd14f91abbfe5e1
samyoshon <samyoshon@gmail.com>
2017-11-05 13:55:11 -0800

    env

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,16 @@
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:    ENV['aws_access_key_id'],
-    aws_secret_access_key:  ENV['aws_secret_access_key'],
-    region:         ENV['aws_region'],
-    endpoint:       ENV['aws_endpoint'],
+    aws_access_key_id:    ENV["aws_access_key_id"],
+    aws_secret_access_key:  ENV["aws_secret_access_key"],
+    region:         ENV["aws_region"],
+    endpoint:       ENV["aws_endpoint"]
   }
-  config.fog_directory  = ENV['aws_bucket']                          # required
+  config.fog_directory  = ENV["aws_bucket"]                          # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
