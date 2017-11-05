5953e1b46c1c1c0cf6e7b940649f180d043af3a4
samyoshon <samyoshon@gmail.com>
2017-11-05 15:04:06 -0800

    migration files

--- db/migrate/20170910190834_add_category_to_photos.rb
+++ db/migrate/20170910190834_add_category_to_photos.rb
@@ -1,4 +1,4 @@
-class AddCategoryIdToProducts < ActiveRecord::Migration[4.2]
+class AddCategoryToProducts < ActiveRecord::Migration[4.2]
   def change
     add_reference :products, :category, index: true, foreign_key: true
     add_reference :products, :market, index: true, foreign_key: true
