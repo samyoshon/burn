812147bb54731ee8cb1bfa717a539854eb214fa6
samyoshon <samyoshon@gmail.com>
2017-11-05 14:28:22 -0800

    capitalize

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,16 +3,16 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  # config.root = Rails.root.join('tmp') # adding these...
-  # config.cache_dir = 'carrierwave' # ...two lines
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
 
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:    ENV["aws_access_key_id"],
-    aws_secret_access_key:  ENV["aws_secret_access_key"],
-    region:         ENV["aws_region"],
-    endpoint:       ENV["aws_endpoint"]
+    aws_access_key_id:    ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key:  ENV["AWS_SECRET_ACCESS_KEY"],
+    region:         ENV["AWS_REGION"],
+    endpoint:       ENV["AWS_ENDPOINT"]
   }
 
   # For testing, upload files to local `tmp` folder.
@@ -22,10 +22,8 @@ CarrierWave.configure do |config|
     config.root = "#{Rails.root}/tmp"
   end
 
-  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
-
-  config.fog_directory  = ENV["aws_bucket"]                          # required
-  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+  config.fog_directory  = ENV["AWS_BUCKET"]                          # required
+  # config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
