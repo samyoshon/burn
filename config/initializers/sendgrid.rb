ee6ef8f74d337a91d469be908b2ea32228a79157
samyoshon <samyoshon@gmail.com>
2017-11-13 21:57:36 -0800

    emails

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,22 +1,27 @@
-if Rails.env.development? 
-	ActionMailer::Base.smtp_settings = {
-	  :user_name => 'apikey',
-	  :password => ENV["SENDGRID_API_KEY_DEV"],
-	  :domain => ENV["SENDGRID_DOMAIN"],
-	  :address => ENV["SENDGRID_API_KEY_PROD"],
-	  :port => 587,
-	  :authentication => :plain,
-	  :enable_starttls_auto => true
-	}                          # required
-elsif Rails.env.production?
-	ActionMailer::Base.smtp_settings = {
-	  :user_name => 'apikey',
-	  :password => ENV["SENDGRID_API_KEY_PROD"],
-	  :domain => ENV["SENDGRID_DOMAIN"],
-	  :address => ENV["SENDGRID_API_KEY_PROD"],
-	  :port => 587,
-	  :authentication => :plain,
-	  :enable_starttls_auto => true
-	}               
-end 
+if Rails.env.development?
 
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  user_name:            ENV["SENDGRID_USERNAME_DEV"],
+  password:             ENV["SENDGRID_PASSWORD_DEV"],
+  domain:               ENV["SENDGRID_DOMAIN_DEV"],
+  address:              ENV["SENDGRID_ADDRESS_DEV"],
+  port:                 587,
+  authentication:       'plain',
+  enable_starttls_auto: true 
+}
+
+elsif Rails.env.production?
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  user_name:            ENV["SENDGRID_USERNAME_PROD"],
+  password:             ENV["SENDGRID_PASSWORD_PROD"],
+  domain:               ENV["SENDGRID_DOMAIN_PROD"],
+  address:              ENV["SENDGRID_ADDRESS_PROD"],
+  port:                 587,
+  authentication:       'plain',
+  enable_starttls_auto: true 
+}
+
+end
